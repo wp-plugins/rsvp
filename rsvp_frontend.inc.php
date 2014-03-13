2d74df1447804f3236b71e03e84b35d45a8ed336
mdedev <mdedev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-13 00:43:50 +0000

    Changes for 1.7.9 see change log for details.
    git-svn-id: https://plugins.svn.wordpress.org/rsvp/trunk@874445 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- rsvp_frontend.inc.php
+++ rsvp_frontend.inc.php
@@ -373,7 +373,7 @@ function rsvp_frontend_main_form($attendeeID, $rsvpStep = "handleRsvp") {
 														\"<label for=\\\"newAttending\" + numAdditional + \"VeggieMealN\\\">$noText</label>\" + 
 													\"</div>\" + ";
 												}
-												$tmpVar = str_replace("\r\n", "", str_replace("|", "\"", addSlashes(rsvp_buildAdditionalQuestions(0, "| + numAdditional + |"))));
+												$tmpVar = str_replace("\r\n", "", str_replace("|", "\"", addSlashes(rsvp_buildAdditionalQuestions($attendeeID, "| + numAdditional + |"))));
 												
 												$form .= "\"".$tmpVar."\" + 
                           \"<p><button onclick=\\\"removeAdditionalRSVP(this);\\\">Remove Guest</button></p>\" + 
